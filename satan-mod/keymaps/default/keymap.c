b64570fd9695da36ad46f1bf5fdda6558f6439a1
GallaFrancesco <francesco.galla3@gmail.com>
2017-05-01 18:50:54 +0200

    new bsls

--- satan-mod/keymaps/default/keymap.c
+++ satan-mod/keymaps/default/keymap.c
@@ -34,15 +34,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,KC_6   ,KC_7   ,KC_8	 ,KC_9   ,KC_0   ,KC_MINS,KC_EQL ,KC_BSLS ,\
   KC_TAB ,KC_Q   ,KC_W   ,KC_F   ,KC_P   ,KC_G   ,KC_LBRC,KC_J   ,KC_L	 ,KC_U   ,KC_Y   ,KC_SCLN,KC_QUOT,KC_BSPC ,\
   KC_ESC ,KC_A   ,KC_R   ,KC_S   ,KC_T   ,KC_D   ,KC_RBRC,KC_H   ,KC_N	 ,KC_E   ,KC_I   ,KC_O   ,KC_ENT		  ,\
-  KC_LSFT		 ,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_SLSH,KC_K	 ,KC_M   ,KC_COMM,KC_DOT ,MO(_FL)		  ,\
-  KC_LCTL,KC_LGUI,MO(_FL),                KC_SPC ,                                        MO(_FL),KC_RGUI,TG(_NL),TG(_QL)),
+  KC_LSFT		 ,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_SLSH,KC_K	 ,KC_M   ,KC_COMM,KC_DOT ,KC_BSLS	  ,\
+  KC_LCTL,KC_LGUI,MO(_FL),   		KC_SPC,                            LCTL(KC_C),LCTL(KC_V),TG(_NL),TG(_QL)),
 
 [_FL] = KEYMAP_ANSI( // function layer
   KC_GRV ,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4   ,KC_F5  ,KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10  ,KC_F11 ,KC_F12 ,RESET,  \
-  _______,_______,_______,KC_UP  ,_______ ,_______,KC_LPRN,KC_RPRN,_______,KC_UP  ,KC_HOME ,KC_END ,BL_INC ,KC_DEL, \
-  _______,KC_LALT,KC_LEFT,KC_DOWN,KC_RIGHT,_______,_______,_______,KC_LEFT,KC_DOWN,KC_RIGHT,_______,_______, \
+  _______,_______,_______,KC_UP  ,_______ ,_______,KC_LPRN,_______,_______,KC_UP  ,KC_HOME ,KC_END ,BL_INC ,KC_DEL, \
+  _______,KC_LALT,KC_LEFT,KC_DOWN,KC_RIGHT,_______,KC_RPRN,_______,KC_LEFT,KC_DOWN,KC_RIGHT,_______,_______, \
   _______,_______,_______,_______,_______ ,_______,_______,KC_MINS,KC_EQL ,KC_PGUP,KC_PGDN ,_______,
-  _______,_______,_______,                 _______,                                _______,_______ ,_______,_______),
+  _______,_______,_______,                 _______,                                LCTL(LSFT(KC_C)),LCTL(LSFT(KC_V)),_______,_______),
 
 [_NL] = KEYMAP_ANSI( // numpad and slide/pdf layer
   _______,_______,_______,_______,_______ ,_______,KC_PGUP,KC_7,KC_8  ,KC_9   ,KC_SLSH,_______,_______,_______, \
